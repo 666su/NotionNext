6b631f45c0ed31961b1175e34b1ebce098d6f27a
suxun <2210940112@tiangong.edu.cn>
2026-08-02 14:20:09 +0800

    Update Header.js

--- themes/example/components/Header.js
+++ themes/example/components/Header.js
@@ -1,21 +1,25 @@
 import { siteConfig } from '@/lib/config'
+import DarkModeButton from '@/components/DarkModeButton'
 import SmartLink from '@/components/SmartLink'
 import { MenuList } from './MenuList'
-import DarkModeButton from '@/components/DarkModeButton'
+
 
 export const Header = props => {
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-white/80 backdrop-blur dark:bg-black/80'>
+
       <div className='mx-auto flex h-14 max-w-6xl items-center justify-between px-6'>
 
-        {/* 左侧 Logo + 头像 */}
+
+        {/* 左侧：头像 + 名称 */}
         <div className='flex items-center gap-3'>
 
           <img
             src='/images.jpg'
-            className='w-8 h-8 rounded-full'
-            alt='avatar'
+            className='w-9 h-9 rounded-full'
+            alt='Jason'
           />
+
 
           <SmartLink
             href='/'
@@ -26,27 +30,65 @@ export const Header = props => {
         </div>
 
 
+
         {/* 中间导航 */}
         <div className='flex-1 flex justify-center'>
+
           <MenuList {...props} />
+
         </div>
 
 
-        {/* 右侧功能 */}
-        <div className='flex items-center gap-3'>
 
+
+        {/* 右侧功能 */}
+        <div className='flex items-center gap-4'>
+
+
+          {/* 语言 */}
+          <SmartLink
+            href='/'
+            className='text-sm hover:text-blue-500'>
+            English
+          </SmartLink>
+
+
+
+          {/* 夜间模式 */}
           <DarkModeButton />
 
+
+
+          {/* 搜索 */}
           <SmartLink
             href='/search'
-            className='text-gray-600 hover:text-black dark:text-gray-300'>
+            className='hidden md:flex items-center gap-2 
+            border rounded-md px-3 py-1.5
+            text-sm text-gray-500
+            hover:border-gray-400'>
+
             <i className='fas fa-search' />
+
+            <span>
+              Search
+            </span>
+
+            <kbd
+              className='px-1.5 py-0.5 text-xs 
+              border rounded bg-gray-100 
+              dark:bg-gray-800'>
+              Ctrl K
+            </kbd>
+
           </SmartLink>
+
+
 
         </div>
 
 
       </div>
+
     </header>
   )
 }
